311465ec354cf9919dd4ce5e54bb14b36b1cd9a1
Vitor Silva <vitorsilva.com@gmail.com>
2026-01-24 10:11:51 +0000

    chore

--- ralph.sh
+++ ralph.sh
@@ -9,17 +9,17 @@ for ((i=1; i<=ITERATIONS; i++)); do
   # We call 'claude' and feed it the PRD and Progress file as context
   claude --permission-mode acceptEdits --allowedTools "Bash(npm test:*)" "Bash(git:*)" "Bash(npm run test)" --add-dir . -p "
     @docs\learning\epic04_feature_enhancement\features\FEATURE_1.2_NEW_BADGE.md 
-    @docs\learning\epic04_feature_enhancement\features\FEATURE_1.1_progress.md
+    @docs\learning\epic04_feature_enhancement\features\FEATURE_1.2_progress.md
 
     1. Read @docs\learning\epic04_feature_enhancement\features\FEATURE_1.2_NEW_BADGE.md  and look in the ## implementation order for the task 'not started' with the lowest number
     2. Make sure you understand the task fully
-    3. Make sure you are on the FEATURE_1.1_FAVORITE branch in git if not, create worktree and switch to it.
+    3. Make sure you are on the FEATURE_1.2_NEW_BADGE branch in git if not, create worktree and switch to it.
     4. Implement ONLY that task
     5. Run unit test and linting to verify your work.
     6. If tests pass: 
       - Update @docs\learning\epic04_feature_enhancement\features\FEATURE_1.2_NEW_BADGE.md status to 'done'.
       - Append learning notes with errors / problems / fixes / workarounds to @docs\learning\epic04_feature_enhancement\features\FEATURE_1.2_NEW_BADGE_LEARNING_NOTES.md
-      - Append a summary of what you did to @docs\learning\epic04_feature_enhancement\features\FEATURE_1.1_progress.md.
+      - Append a summary of what you did to @docs\learning\epic04_feature_enhancement\features\FEATURE_1.2_progress.md.
       - Commit the changes with a clear message.
     7. if test not pass:
       - Debug and fix the issue.
